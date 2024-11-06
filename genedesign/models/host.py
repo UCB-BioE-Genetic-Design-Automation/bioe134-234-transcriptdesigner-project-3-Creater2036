c60364b1797959e1dd71732b5e3293b4e167ac8b
Ali Tout <at4882@gmail.com>
2024-11-06 04:02:48 -0800

    Delete genedesign directory

--- genedesign/models/host.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class Host(Enum):
-    """
-    The Host is the organism into which DNAs will be inserted or removed.
-    """
-    Ecoli = "Escherichia coli"      # A strain of Escherichia coli
-    Scerevisiae = "Saccharomyces cerevisiae"  # A strain of Saccharomyces cerevisiae
